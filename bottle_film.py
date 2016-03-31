e8461aa97db4e56b6c9975d9e6a57d70ea73a145
aalfredsson <aalfredsson94@gmail.com>
2016-03-31 13:32:54 +0200

    Uppdaterad

--- bottle_film.py
+++ bottle_film.py
@@ -7,11 +7,7 @@ def index():
 
 @route('/static/<filename>')
 def server_static(filename):
-	'''Handles the routes to our static files
 
-	Returns:
-		file : the static file requested by URL
-	'''
 	return static_file(filename, root="static")
     
 run(host="localhost", port=8080, debug=True)
